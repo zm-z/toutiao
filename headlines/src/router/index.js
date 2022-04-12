ee300163b1bd9bdaf7a76e8db0bd613e7fde2658
zzm <2846909278@qq.com>
2022-04-12 19:45:33 +0800

    sqhdsqdhq

--- headlines/src/router/index.js
+++ headlines/src/router/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import Router from 'vue-router'
 import login from '@/components/Login'
 console.log(11111111);
+console.log(2222222);
 const home = () =>
     import ('@/components/Home')
 const image = () =>
