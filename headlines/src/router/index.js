95e10c1e71c6e7a622ad5483247c02b010cbb2d8
zzm <2846909278@qq.com>
2022-04-12 20:42:58 +0800

    修改bug

--- headlines/src/router/index.js
+++ headlines/src/router/index.js
@@ -2,9 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 import Router from 'vue-router'
 import login from '@/components/Login'
-console.log(11111111);
-console.log(2222222);
-console.log(2222222);
+console.log('zzm');
 const home = () =>
     import ('@/components/Home')
 const image = () =>
